9cb4703af5e4ce8d984aa78cccf28ca6b607d141
Mykhailo Pashchenkoe <bdemlolp234@gmail.com>
2023-09-30 12:43:42 +0300

    added task2 and created translator

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 # Task1
 
-txt_dict = {}
-text = input("text= ")
+# txt_dict = {}
+# text = input("text= ")
+#
+# txt_dict[text.count(text)] = text
+# print(txt_dict)
 
-txt_dict[text.count(text)] = text
-print(txt_dict)
+#Task2
+translations = {
+    'hi': 'привіт',
+    'book': 'книга',
+    'house': 'дім',
+    'car': 'машина'
+}
+word = input("= ")
+if word not in translations:
+    print("В словнику немає такого слова")
+else:
+
+    get_translate = translations.get(word)
+    print(get_translate)
